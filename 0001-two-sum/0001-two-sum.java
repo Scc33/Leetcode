da68bf033e8841f27dec0d4b80a1a6aab5ee3803
Sean Coughlin <seanmc4@illinois.edu>
2023-04-10 10:57:54 -0500

    Time: 85 ms (25.86%), Space: 42.5 MB (80.63%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                    if (i != j && nums[i] + nums[j] == target) {
+                        ans[0] = i;
+                        ans[1] = j;
+                        return ans;
+                    }
+                }
+            }
+        return ans;
+    }
+}
