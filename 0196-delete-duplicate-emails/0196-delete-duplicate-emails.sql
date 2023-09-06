a85e8dc87962377258b2608d69dc2521a7fa82f3
Sean Coughlin <seanmc4@illinois.edu>
2023-09-05 21:05:59 -0500

    Time: 1992 ms (25.67%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-delete p1 from person p1,person p2 
-where p1.email=p2.email and p1.id>p2.id;
+DELETE p1 
+FROM person p1,person p2 
+WHERE p1.email=p2.email AND p1.id>p2.id;
