0aa037f60423ad7f55013e7a4921869bad299559
Sean Coughlin <seanmc4@illinois.edu>
2023-09-07 09:39:20 -0500

    Time: 33 ms (69.29%), Space: 17 MB (13.86%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n == 0:
+            return [[]]
+        return self.dfs(1, n+1)
+        
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        result = []
+        for i in range(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i+1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right  = l, r
+                    result.append(node)
+        return result
+        
