a3066be2b2122684d978cc6a015ca47f779b8bb4
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-28 18:02:21 -0500

    Time: 0 ms (100%), Space: 56.5 MB (93.39%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root == null) {
+        return 0;
+    } else {
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
+};
