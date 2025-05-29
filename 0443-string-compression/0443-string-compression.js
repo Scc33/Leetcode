c41f0913c4e43fe204589cb93733b124a7fae46c
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-29 17:36:20 -0500

    Time: 49 ms (5.2%), Space: 61.9 MB (5.07%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,26 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let compressionPointer = 0;
+    let placingPointer = 0;
+    while (compressionPointer < chars.length) {
+        const letter = chars[compressionPointer];
+        let count = 1;
+        while (letter === chars[++compressionPointer]) {
+            count++;
+        }
+        if (count === 1) {
+            chars[placingPointer] = letter;
+            placingPointer++;
+        } else {
+            chars[placingPointer] = letter;
+            const countChars = [...String(count)];
+            console.log(countChars)
+            chars.splice(placingPointer+1, countChars.length, ...countChars);
+            placingPointer += 1 + countChars.length;
+        }
+    }
+    return placingPointer;
+};
