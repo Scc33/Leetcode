62a813e09c55a4384495b4c559ea9458bfe244d4
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 13:18:14 -0500

    Time: 56 ms (29.25%), Space: 57.7 MB (81.94%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.ts
@@ -0,0 +1,21 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+    let count = init;
+    return {
+        increment: () => { return ++count; },
+        decrement: () => { return --count; },
+        reset: () => { count = init; return count; }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
