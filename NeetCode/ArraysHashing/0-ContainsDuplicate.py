ac2475783ca9673ca5b5549e31409404dc7835c4
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 12:04:35 -0500

    Create 0-ContainsDuplicate.py

---
+++ NeetCode/ArraysHashing/0-ContainsDuplicate.py
@@ -0,0 +1,10 @@
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
