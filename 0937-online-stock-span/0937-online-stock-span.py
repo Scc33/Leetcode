e4760b8dd107a45f10ebdb6eed354a009fc86889
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 14:36:10 -0500

    Time: 35 ms (98.46%), Space: 23 MB (17.74%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -1,29 +1,24 @@
 class StockSpanner:
 
     def __init__(self):
-        
         # maintain a monotonic stack for stock entry
-        
 		## definition of stock entry:
         # first parameter is price quote
         # second parameter is price span
         self.monotone_stack = []
         
-    def next(self, price: int) -> int:
-
-        stack = self.monotone_stack
-        
+    def next(self, price: int) -> int:        
         cur_price_quote, cur_price_span = price, 1
         
         # Compute price span in stock data with monotonic stack
-        while stack and stack[-1][0] <= cur_price_quote:
+        while self.monotone_stack and self.monotone_stack[-1][0] <= cur_price_quote:
             
-            prev_price_quote, prev_price_span = stack.pop()
+            prev_price_quote, prev_price_span = self.monotone_stack.pop()
             
             # update current price span with history data in stack
             cur_price_span += prev_price_span
         
         # Update latest price quote and price span
-        stack.append( (cur_price_quote, cur_price_span) )
+        self.monotone_stack.append( (cur_price_quote, cur_price_span) )
         
         return cur_price_span
