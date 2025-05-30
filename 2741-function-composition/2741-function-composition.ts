28a0e84569f812a1a875ecbc4fe68d70cd6c119d
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 14:48:23 -0500

    Time: 51 ms (83.76%), Space: 57.4 MB (8.97%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.ts
@@ -0,0 +1,20 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    
+    return function(x) {
+        if (functions.length === 0) {
+            return x;
+        } else {
+            for (let i = functions.length - 1; i >=0; i--) {
+                x = functions[i](x);
+            }
+            return x;
+        }
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
