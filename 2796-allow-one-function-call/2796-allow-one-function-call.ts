a879c8a6c4e0b1afde4bd88509bb457bca134f79
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 20:45:24 -0500

    Time: 43 ms (65.41%), Space: 54.6 MB (79.07%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,22 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let callable = true;
+    return function (...args) {
+        if (callable) {
+            callable = false;
+            return fn(...args);
+        } else {
+            return undefined;
+        }
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
