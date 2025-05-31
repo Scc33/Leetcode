45d46028c2b6af63a09c27ca8027bc168c202a8c
Sean Coughlin <coughlinscc33@gmail.com>
2025-05-30 20:54:13 -0500

    Time: 36 ms (98.92%), Space: 53.1 MB (93.4%) - LeetHub

---
+++ 2859-add-two-promises/2859-add-two-promises.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    return Promise.all([promise1,promise2])
+           .then( ([result1,result2]) => result1 + result2);
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
