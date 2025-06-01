6c105543a3615354bd5a8d479864b8cf5d7135eb
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-01 12:39:04 -0500

    Time: 271 ms (43.15%), Space: 96 MB (43.15%) - LeetHub

---
+++ 2731-memoize/2731-memoize.ts
@@ -0,0 +1,28 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+
+    let memoized = {};
+    
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (key in memoized) {
+            return memoized[key];
+        }
+        const result = fn.apply(this, args);
+        memoized[key] = result;
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
