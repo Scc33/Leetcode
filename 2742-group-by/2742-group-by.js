8493ac4ae4aa64341b9778fcc137112fc08bd7a4
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-01 11:52:18 -0500

    Time: 107 ms (38.43%), Space: 80.2 MB (11.08%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let grouping = {};
+    for (const obj of this) {
+        let key = fn(obj);
+        if (!grouping[key]) {
+            grouping[key] = [];
+        }
+        grouping[key].push(obj)            
+    }
+    return grouping;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
