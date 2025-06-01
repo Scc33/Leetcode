a3e493b5f3e9ff6651fb02edf28ab6556206db8b
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-01 12:11:13 -0500

    Time: 43 ms (58.87%), Space: 55.2 MB (40.84%) - LeetHub

---
+++ 2863-calculator-with-method-chaining/2863-calculator-with-method-chaining.ts
@@ -0,0 +1,38 @@
+class Calculator {
+    
+    val: number;
+
+    constructor(value: number) {
+        this.val = value;
+    }
+    
+    add(value: number): Calculator {
+        this.val += value;
+        return this;
+    }
+    
+    subtract(value: number): Calculator {
+        this.val -= value;
+        return this;
+    }
+    
+    multiply(value: number): Calculator {
+        this.val *= value;
+        return this;
+    }
+    
+    divide(value: number): Calculator {
+        if(value === 0) throw "Division by zero is not allowed";
+        this.val /= value;
+        return this;
+    }
+    
+    power(value: number): Calculator {
+        this.val **= value;
+        return this;
+    }
+    
+    getResult(): number {
+        return this.val;
+    }
+}
