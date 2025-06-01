a8077bb7084d922e6ccb53a89818bc8ecfce539a
Sean Coughlin <coughlinscc33@gmail.com>
2025-06-01 12:21:06 -0500

    Time: 56 ms (82.49%), Space: 56.5 MB (26.52%) - LeetHub

---
+++ 2807-execute-asynchronous-functions-in-parallel/2807-execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,23 @@
+type Fn<T> = () => Promise<T>
+
+function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+        const results = new Array(functions.length);
+        let count = 0;
+        functions.forEach((fn, i) => {
+            fn().then(val => {
+                results[i] = val;
+                count ++
+                if (count === functions.length) {
+                    resolve(results);
+                }
+            })
+            .catch(reason => reject(reason))
+        });
+    });
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
